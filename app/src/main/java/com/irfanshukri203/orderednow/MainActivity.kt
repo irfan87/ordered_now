f51ef4bf0312b8ae82e4e0f705f30b3e6906ebc6
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-13 23:55:12 +0800

    make a simple form layout

--- app/src/main/java/com/irfanshukri203/orderednow/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/orderednow/MainActivity.kt
@@ -2,10 +2,24 @@ package com.irfanshukri203.orderednow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOrder.setOnClickListener {
+            val itemName = etItemName.text.toString()
+            val itemQuantity = etItemQuantity.text.toString().toInt()
+            val checkedDeliveryType = rgDelivery.checkedRadioButtonId
+            val deliveryType = findViewById<RadioButton>(checkedDeliveryType)
+            val deliveryString = """You have ordered:
+                | $itemName ($itemQuantity items). 
+                | You decided to use ${deliveryType.text} option""".trimMargin()
+
+            tvResult.text = deliveryString
+        }
     }
 }
